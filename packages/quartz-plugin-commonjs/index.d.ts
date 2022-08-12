c1118b6e5c31ce1333be434a4a21bbaf8bffd457
yellowsink <yellowsink@riseup.net>
2022-08-12 17:20:18 +0100

    quartz-plugin-commonjs

---
+++ packages/quartz-plugin-commonjs/index.d.ts
@@ -0,0 +1,5 @@
+declare module "quartz-plugin-commonjs";
+import { QuartzPlugin } from "@uwu/quartz";
+
+declare const _default: () => QuartzPlugin;
+export default _default;
---
+++ packages/quartz-plugin-commonjs/index.js
@@ -0,0 +1,23 @@
+// ignore if export anywhere, or import thats not dynamic (followed by not `(`)
+const ignoreRegex = /export\s+|import\s*?[^(]/g;
+const constRequireRegex = /const\s+(.*?)\s*=\s*require\("(.*?)"\)/g;
+const lvRequireRegex = /(let|var)\s+(.*?)\s*=\s*require\("(.*?)"\)/g;
+const moduleExportsRegex = /module.exports\s*=/g;
+
+let count = 0;
+
+export default () => ({
+  transform: ({ code }) =>
+    code.match(ignoreRegex)
+      ? undefined
+      : code
+          // replace const requires to simple imports
+          .replaceAll(constRequireRegex, "import $1 from \"$2\";")
+          // replace let/var requires to imports and then local copies
+          .replaceAll(
+            lvRequireRegex,
+            `import ___${count++} from \"$3\"; $1 $2 = ___${count}`
+          )
+          // replace module.exports to export default
+          .replaceAll(moduleExportsRegex, "export default "),
+});
