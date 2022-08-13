881765fc182e47cb32f2ed38aae8e6fe8a8701e1
yellowsink <yellowsink@riseup.net>
2022-08-13 13:40:33 +0100

    fix the regex (thanks kasi)

--- packages/quartz-plugin-commonjs/index.js
+++ packages/quartz-plugin-commonjs/index.js
@@ -1,7 +1,7 @@
 // ignore if export anywhere, or import thats not dynamic (followed by not `(`)
 const ignoreRegex = /export\s+|import\s*?[^(]/g;
-const constRequireRegex = /const\s+(.*?)\s*=\s*require\("(.*?)"\)/g;
-const lvRequireRegex = /(let|var)\s+(.*?)\s*=\s*require\("(.*?)"\)/g;
+const constRequireRegex = /const\s+([^;]*?)\s*=\s*require\("(.*?)"\)/gm;
+const lvRequireRegex = /(let|var)\s+([^;]*?)\s*=\s*require\("(.*?)"\)/gm;
 const moduleExportsRegex = /module.exports\s*=/g;
 
 let count = 0;
